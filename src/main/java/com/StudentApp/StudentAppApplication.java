014292e5db7d16eeb15967458b9e119949db7971
akshay jagtap <akshayjagtap2000@gmail.com>
2025-12-13 21:24:47 +0530

    Controller Added

--- src/main/java/com/StudentApp/StudentAppApplication.java
+++ src/main/java/com/StudentApp/StudentAppApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentAppApplication {
 
 	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		SpringApplication.run(StudentAppApplication.class, args);
 	}
 
